6053d37bdc6c20d271128ad240694ab5841902ae
MohamedMoustafa44 <120092475+MohamedMoustafa44@users.noreply.github.com>
2023-03-22 16:47:14 +0200

    Generating exam questions as lables, and ComboBoxes for choices in progress

--- ExaminationSystem/ExamForm.cs
+++ ExaminationSystem/ExamForm.cs
@@ -28,6 +28,7 @@ namespace ExaminationSystem
             stdId = i;
             exId = k;
             crsId = j;
+            this.AutoScroll = true;
         }
 
         private void ExamForm_Load(object sender, EventArgs e)
@@ -35,7 +36,7 @@ namespace ExaminationSystem
             var obj = context.Student_Exam_Question
                 .Where(ob => ob.Std_id == stdId && ob.ex_id == exId);
             int x = 30;
-            int y = 0;
+            int y = 50;
             int i = 0;
             foreach(var ob in obj)
             {
@@ -49,27 +50,18 @@ namespace ExaminationSystem
                 Label lb = new Label();
                 #region labelProperties
                 lb.AutoSize = true;
-                lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-                lb.Location = new System.Drawing.Point(x, y);
+                lb.Font = new Font("Microsoft Sans Serif",10F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
+                lb.Location = new Point(x, y);
                 lb.Name = $"label{i}";
-                lb.Size = new System.Drawing.Size(218, 32);
+                lb.Size = new Size(218, 32);
                 lb.TabIndex = 28;
                 lb.Text = q.question1;
                 #endregion
                 labels.Add(lb);
                 y += 100;
                 i++;
+                this.Controls.Add(lb);
             }
-
-
-            //this.CourseBox.FormattingEnabled = true;
-            //this.CourseBox.Location = new System.Drawing.Point(673, 51);
-            //this.CourseBox.Name = "CourseBox";
-            //this.CourseBox.Size = new System.Drawing.Size(252, 24);
-            //this.CourseBox.TabIndex = 26;
-
-
-
         }
     }
 }
